a779c60786f5831d07f5d4ed3245ce73d6c6d170
Eliel-Doux <eliel.doux@edu.esiee.fr>
2024-11-07 10:34:00 +0000

    Eliel Doux G9 ex palyndrome

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 #### Fonction secondaire
-
-
+'''on importe unidecode'''
+import string
+from unidecode import unidecode
 def ispalindrome(p):
-
-    # votre code ici
-    
+    '''fonction'''
+    p = unidecode(p.replace(" ", "")).lower()
+    p = p.translate(str.maketrans("", "", string.punctuation))
+    if p == p[::-1]:
+        return True
     return False
-
 #### Fonction principale
-
-
 def main():
-
+    '''fonction'''
     # vos appels à la fonction secondaire ici
-
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
-
 if __name__ == "__main__":
     main()
